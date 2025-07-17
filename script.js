03025b5a4e6a283baecd585be7a1e8b8ec0e62df
Aryamix <amiraryamix@gmail.com>
2025-07-17 17:29:26 +0330

    add
    add files

---
+++ script.js
@@ -0,0 +1,50 @@
+//const registerButton = document.querySelector('.register-btn');
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//const loginButton = document.querySelector('.login-btn');
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//if (registerButton) {
+//    registerButton.addEventListener('click', function(event) {
+//        event.preventDefault(); // جلوگیری از انجام عمل پیش‌فرض (مثلا ارسال فرم)
+ //       window.location.href = 'register.html'; // انتقال به صفحه ثبت‌نام
+  //  });
+//}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+//if (loginButton) {
+ //   loginButton.addEventListener('click', function(event) {
+ //       event.preventDefault(); // جلوگیری از انجام عمل پیش‌فرض
+  //      window.location.href = 'login.html'; // انتقال به صفحه ورود
+  //  });
+///}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//const safheasli = document.querySelector('.safheaslo');
+
+
+//if (safheasli) {
+  //  safheasli.addEventListener('click', function(event) {
+  //      event.preventDefault(); // جلوگیری از انجام عمل پیش‌فرض
+  //      window.location.href = 'am.html'; // انتقال به صفحه ورود
+  //  });
+//}
+
